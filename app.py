460d1fc61ecb6816026159074057e56e15b1a0c2
Gabriel Carmo <gabriellcarmo.dev@gmail.com>
2026-06-26 08:24:35 -0300

    configuracao para deploy no Railway

--- app.py
+++ app.py
@@ -100,5 +100,7 @@ def server_error(e):
 
 
 if __name__ == '__main__':
+    import os
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
